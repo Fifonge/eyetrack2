51fdb06b45df013b69071e73acc9065b2d3ad3bf
Fifonge <szumilemilo@gmail.com>
2026-02-16 17:37:04 +0100

    Add files via upload

---
+++ scripts/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"i","comments":{"zero":"dodaj komentarz","multiple":"{num} komentarzy","one":"1 komentarz"}},"counts":[{"id":"https:\/\/naukaoklimacie.pl\/fakty-i-mity\/mit-nadchodzace-przebiegunowanie-jest-przyczyna-zmiany-klimatu","comments":0},{"id":"https:\/\/naukaoklimacie.pl\/fakty-i-mity\/mit-dwutlenek-wegla-emitowany-przez-czlowieka-nie-ma-znaczenia-31","comments":337},{"id":"https:\/\/naukaoklimacie.pl\/fakty-i-mity\/mit-meteorolodzy-i-telewizje-zmieniaja-skale-kolorow-na-mapach-pogody-zeby-siac-panike","comments":0}]});
+}
---
+++ scripts/recent_comments_widget.js
@@ -0,0 +1,109 @@
+
+
+document.write(' \
+<style type="text/css" media="screen">\
+	 .dsq-widget ul.dsq-widget-list {\
+	 padding: 0;\
+	 margin: 0;\
+	 text-align: left;\
+	 }\
+	 img.dsq-widget-avatar {\
+	 width: 32px;\
+	 height: 32px;\
+	 border: 0px;\
+	 margin: 0px;\
+	 padding: 0px 3px 3px 0px;\
+	 float: left;\
+	 }\
+	 a.dsq-widget-user {\
+	 font-weight: bold;\
+	 }\
+	 a.dsq-widget-thread {\
+	 font-weight: bold;\
+	 }\
+	 p.dsq-widget-meta {\
+	 clear: both;\
+	 font-size: 80%;\
+	 padding: 0;\
+	 margin: 0;\
+	 }\
+	 li.dsq-widget-item {\
+	 margin: 15px 0;\
+	 list-style-type: none;\
+	 clear: both;\
+	 }\
+	 span.dsq-widget-clout {\
+	 padding: 0 2px;\
+	 background-color: #ff7300;\
+	 color: #fff;\
+	 }\
+	 table.dsq-widget-horiz td {\
+	 padding-right: 15px;\
+	 }\
+	 .dsq-widget-comment p {\
+	 display: inline;\
+	 }\
+	 </style>\
+	 <ul class="dsq-widget-list">\
+	 <li class="dsq-widget-item">\
+	 <a href="https://disqus.com/by/disqus_Z2MaL8n1J5/"><img class="dsq-widget-avatar" src="//a.disquscdn.com/1770237905/images/noavatar92.png"></a>\
+	 <a class="dsq-widget-user" href="https://disqus.com/by/disqus_Z2MaL8n1J5/">Dariusz Młotkowski</a>\
+	 <span class="dsq-widget-comment"><blockquote>Ile z ostatnich 10 zim było śnieżnych?</blockquote><p> Holendrzy zapytają "a ile ma być ?" Pozdro dla kumatych. Na pewno Holendrzy podzielają się chętnie informacjami odnośnie...</p></span>\
+	 <p class="dsq-widget-meta"><a href="http://naukaoklimacie.pl/fakty-i-mity/mit-rekordowe-opady-sniegu-to-dowod-na-to-ze-globalne-ocieplenie-to-fikcja-62">Mit: Rekordowe opady śniegu to dowód na to, że globalne ocieplenie to fikcja | naukaoklimacie.pl</a>&nbsp;&middot;&nbsp;<a href="http://naukaoklimacie.pl/fakty-i-mity/mit-rekordowe-opady-sniegu-to-dowod-na-to-ze-globalne-ocieplenie-to-fikcja-62#comment-6838623546">3 minutes ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="https://disqus.com/by/disqus_Pipc0dQsyQ/"><img class="dsq-widget-avatar" src="//a.disquscdn.com/1770237905/images/noavatar92.png"></a>\
+	 <a class="dsq-widget-user" href="https://disqus.com/by/disqus_Pipc0dQsyQ/">Bartosz</a>\
+	 <span class="dsq-widget-comment"><p>Poprawcie tego chochlika, co Wam zamienił 2,5 na 1,5, bo w takim artykule to nie wypada. ;) .</p></span>\
+	 <p class="dsq-widget-meta"><a href="https://naukaoklimacie.pl/aktualnosci/praca-dla-detektywa-jak-sprawdzic-dlaczego-ziemia-sie-ociepla">Praca dla detektywa: jak sprawdzić dlaczego Ziemia się ociepla?</a>&nbsp;&middot;&nbsp;<a href="https://naukaoklimacie.pl/aktualnosci/praca-dla-detektywa-jak-sprawdzic-dlaczego-ziemia-sie-ociepla#comment-6838579434">1 hour ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="https://disqus.com/by/reginastpie/"><img class="dsq-widget-avatar" src="https://c.disquscdn.com/uploads/users/42935614173/6707/avatar92.jpg?1771036539"></a>\
+	 <a class="dsq-widget-user" href="https://disqus.com/by/reginastpie/">Regina Stępień</a>\
+	 <span class="dsq-widget-comment"><p>A skąd trzeba  tu wpaść, żeby nie nie można było powiedzieć: <br>" Wpadłeś tu nie wiadomo skąd"</p></span>\
+	 <p class="dsq-widget-meta"><a href="https://naukaoklimacie.pl/archiwum-faktow-i-mitow/mit-miedzy-temperatura-a-koncentracjami-co2-nie-ma-korelacji-wersja-archiwalna">Mit: Między temperaturą a koncentracjami CO2 nie ma korelacji | naukaoklimacie.pl</a>&nbsp;&middot;&nbsp;<a href="https://naukaoklimacie.pl/archiwum-faktow-i-mitow/mit-miedzy-temperatura-a-koncentracjami-co2-nie-ma-korelacji-wersja-archiwalna#comment-6837682357">2 days ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="https://disqus.com/by/alfa_y/"><img class="dsq-widget-avatar" src="https://c.disquscdn.com/uploads/users/15774/2242/avatar92.jpg?1438271186"></a>\
+	 <a class="dsq-widget-user" href="https://disqus.com/by/alfa_y/">alfa_y</a>\
+	 <span class="dsq-widget-comment"><blockquote>Jak zimy były ciepłe to non stop trąbiliście że to jest wina GO, jak teraz wróciły mroźne i śnieżne zimy no to również jest to wina GO.</blockquote><p> Ile z ostatnich 10 zim było...</p></span>\
+	 <p class="dsq-widget-meta"><a href="http://naukaoklimacie.pl/fakty-i-mity/mit-rekordowe-opady-sniegu-to-dowod-na-to-ze-globalne-ocieplenie-to-fikcja-62">Mit: Rekordowe opady śniegu to dowód na to, że globalne ocieplenie to fikcja | naukaoklimacie.pl</a>&nbsp;&middot;&nbsp;<a href="http://naukaoklimacie.pl/fakty-i-mity/mit-rekordowe-opady-sniegu-to-dowod-na-to-ze-globalne-ocieplenie-to-fikcja-62#comment-6837171394">3 days ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="https://disqus.com/by/the_racjonalista/"><img class="dsq-widget-avatar" src="//a.disquscdn.com/1770237905/images/noavatar92.png"></a>\
+	 <a class="dsq-widget-user" href="https://disqus.com/by/the_racjonalista/">Krzysztof Spławski</a>\
+	 <span class="dsq-widget-comment"><blockquote>(...), jak wróciły mroźne i śnieżne zimy no to również jest to wina GO.</blockquote><p>Dariuszu Młotkowski. Jeżeli otworzysz lodówkę to zapewne znajdziesz tam znacznie więcej jedzenia, niż...</p></span>\
+	 <p class="dsq-widget-meta"><a href="http://naukaoklimacie.pl/fakty-i-mity/mit-rekordowe-opady-sniegu-to-dowod-na-to-ze-globalne-ocieplenie-to-fikcja-62">Mit: Rekordowe opady śniegu to dowód na to, że globalne ocieplenie to fikcja | naukaoklimacie.pl</a>&nbsp;&middot;&nbsp;<a href="http://naukaoklimacie.pl/fakty-i-mity/mit-rekordowe-opady-sniegu-to-dowod-na-to-ze-globalne-ocieplenie-to-fikcja-62#comment-6836093936">6 days ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="https://disqus.com/by/zenek1111/"><img class="dsq-widget-avatar" src="//a.disquscdn.com/1770237905/images/noavatar92.png"></a>\
+	 <a class="dsq-widget-user" href="https://disqus.com/by/zenek1111/">zenek1111</a>\
+	 <span class="dsq-widget-comment"><p>Przecież adwersaż oparł swoją wiedze na źrodłach naukowych bardziej niz Pan :P</p></span>\
+	 <p class="dsq-widget-meta"><a href="https://naukaoklimacie.pl/aktualnosci/o-klimacie-w-dobie-dezinformacji-bezplatny-kurs">O klimacie w dobie dezinformacji – bezpłatny kurs</a>&nbsp;&middot;&nbsp;<a href="https://naukaoklimacie.pl/aktualnosci/o-klimacie-w-dobie-dezinformacji-bezplatny-kurs#comment-6835796325">6 days ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="https://disqus.com/by/gretynka/"><img class="dsq-widget-avatar" src="//a.disquscdn.com/1770237905/images/noavatar92.png"></a>\
+	 <a class="dsq-widget-user" href="https://disqus.com/by/gretynka/">gretynka</a>\
+	 <span class="dsq-widget-comment"><p>Temu nie wierzę. Żeby "Gazeta Wyborcza" ochoczo publikowała wywiady z kapłanami należącymi do mafii.</p></span>\
+	 <p class="dsq-widget-meta"><a href="https://naukaoklimacie.pl/aktualnosci/jak-zimny-byl-styczen-2026-roku">Jak zimny był styczeń 2026 roku?</a>&nbsp;&middot;&nbsp;<a href="https://naukaoklimacie.pl/aktualnosci/jak-zimny-byl-styczen-2026-roku#comment-6835696032">6 days ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="https://disqus.com/by/aekardas/"><img class="dsq-widget-avatar" src="https://c.disquscdn.com/uploads/users/6700/158/avatar92.jpg?1505146815"></a>\
+	 <a class="dsq-widget-user" href="https://disqus.com/by/aekardas/">Aleksandra Kardaś</a>\
+	 <span class="dsq-widget-comment"><p>Ten tekst jest na naszej stronie niemal od początku jej istnienia - od listopada 2013. To, że globalne ocieplenie powoduje wzmożone parowanie i możliwość występowania bardzo silnych opadów to fakt...</p></span>\
+	 <p class="dsq-widget-meta"><a href="http://naukaoklimacie.pl/fakty-i-mity/mit-rekordowe-opady-sniegu-to-dowod-na-to-ze-globalne-ocieplenie-to-fikcja-62">Mit: Rekordowe opady śniegu to dowód na to, że globalne ocieplenie to fikcja | naukaoklimacie.pl</a>&nbsp;&middot;&nbsp;<a href="http://naukaoklimacie.pl/fakty-i-mity/mit-rekordowe-opady-sniegu-to-dowod-na-to-ze-globalne-ocieplenie-to-fikcja-62#comment-6835680544">6 days ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="https://disqus.com/by/aekardas/"><img class="dsq-widget-avatar" src="https://c.disquscdn.com/uploads/users/6700/158/avatar92.jpg?1505146815"></a>\
+	 <a class="dsq-widget-user" href="https://disqus.com/by/aekardas/">Aleksandra Kardaś</a>\
+	 <span class="dsq-widget-comment"><p>Zaraz powinny się pojawić podsumowania za styczeń, ale na razie nie widać, żeby globalnie rzecz biorąc był specjalnie chłodny. Można patrzeć dzień po dniu na Climate Reanalyzer...</p></span>\
+	 <p class="dsq-widget-meta"><a href="https://naukaoklimacie.pl/aktualnosci/jak-zimny-byl-styczen-2026-roku">Jak zimny był styczeń 2026 roku?</a>&nbsp;&middot;&nbsp;<a href="https://naukaoklimacie.pl/aktualnosci/jak-zimny-byl-styczen-2026-roku#comment-6835671952">1 week ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="https://disqus.com/by/disqus_5GbMWR7Ius/"><img class="dsq-widget-avatar" src="//a.disquscdn.com/1770237905/images/noavatar92.png"></a>\
+	 <a class="dsq-widget-user" href="https://disqus.com/by/disqus_5GbMWR7Ius/">Malm</a>\
+	 <span class="dsq-widget-comment"><p>Zadajesz niewygodne pytania. ;)</p></span>\
+	 <p class="dsq-widget-meta"><a href="https://naukaoklimacie.pl/aktualnosci/jak-zimny-byl-styczen-2026-roku">Jak zimny był styczeń 2026 roku?</a>&nbsp;&middot;&nbsp;<a href="https://naukaoklimacie.pl/aktualnosci/jak-zimny-byl-styczen-2026-roku#comment-6835636684">1 week ago</a></p>\
+	 </li>\
+	 </ul>\
+');
